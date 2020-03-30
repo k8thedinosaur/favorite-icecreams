a5c0a839acad934806c1c1e1859f9d21c2f582d9
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-30 14:21:52 -0700

    forgot to add document.ready to make jq functional /0"

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-var icecreamFlavors = ["Green Tea", "Chai Tea", "Vanilla with nuts on top", "Mango sorbet"];
-
-icecreamFlavors.forEach(function(flavor) {
-  // console.log(flavor);
-  var flavor = $(flavor).val();
-  $("#list").append("<li>" + flavor + "</li>");
+$(document).ready(function() {
+  var icecreamFlavors = ["Green Tea", "Chai Tea", "Vanilla with nuts on top", "Mango sorbet"];
+  
+  icecreamFlavors.forEach(function(flavor) {
+    console.log(flavor);
+    $(".list").append("<li>" + flavor + "</li>");
+ 
+  });
 });
